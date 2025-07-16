f70ad90b1f77e7366fc1cd7735163e161abcee46
Indra Zulfi Mushoddaq <me@indrazm.com>
2025-07-16 22:50:29 +0700

    reproduce diff proj_id

--- trigger.config.ts
+++ trigger.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@trigger.dev/sdk/v3";
 import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 
 export default defineConfig({
-  project: "proj_rvxecreedhorbvilkoza",
+  project: process.env.TRIGGER_PROJ_ID || "proj_rthwlacfadmleohzuzrt",
   runtime: "node",
   logLevel: "log",
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
